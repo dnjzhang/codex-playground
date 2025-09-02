140ac60e4771c22f6e33aeeb3e45f64c83cd8b8d
jzhang <dn.john.zhang@gmail.com>
2025-09-02 00:14:17 -0400

    First program to use Ollama's LLama3.2 local model & Chroma for a ChatBot

---
+++ ollama-starter/README.txt
@@ -0,0 +1,1 @@
+Sample code to show how to create a LangChain code to work with an Ollama model "llama3.2"
---
+++ ollama-starter/restaurant_review.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are an reviewer in answering questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
+Here is the question to answer: {question}
+
+If you can find the information in the reviews, answer the question. Otherwise, say that you don't know.
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    print("\n\n-------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
+    if question == "q":
+        break
+    
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
+    print(result)
